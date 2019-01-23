84592fa066d1e9b9a70b47ec970eb398f0bde80d
canikwe <c.anikwe@gmail.com>
2019-01-22 19:38:49 -0500

    changed class methods to instance methods

--- bin/run.rb
+++ bin/run.rb
@@ -1,7 +1,7 @@
 require_relative '../config/environment'
 
-
-CLI.welcome
-CLI.get_todays_hero
-CLI.display_menu
+new_game = CLI.new
+new_game.welcome
+new_game.get_todays_hero
+new_game.display_menu
 puts "Go forth and prosper"
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,25 +1,25 @@
 class CLI
   attr_accessor :todays_hero, :enemy
 
-  def self.welcome
+  def welcome
     puts "Welcome to Super Fight Bros\n\n"
   end
 
-  def self.get_todays_hero
+  def get_todays_hero
     puts "Who do you want to be today?"
     input = gets.chomp
     @todays_hero = Superhero.find_by(name: input)
   end
 
-  def self.display_menu
-    puts "Hi #{@todays_hero}! What would you like to do?"
+  def display_menu
+    puts "Hi #{@todays_hero.name}! What would you like to do?"
     puts "***************************"
     puts "1. Start a fight"
     puts "2. Exit"
     self.start_game_loop
   end
 
-  def self.start_game_loop
+  def start_game_loop
     input = gets.chomp
     while
       # input != 2
@@ -43,7 +43,7 @@ class CLI
     end
   end
 
-  def self.start_battle
+  def start_battle
     puts "\n***************************\n"
     puts "\nWho would you like to battle?\n"
     display_villains
@@ -55,20 +55,20 @@ class CLI
   end
 
   # lets the villain say a catchphrase
-  def self.catchprases
+  def catchprases
     puts ["Why so serious?", "Dolts! Such insolence will not go unpunished!", "Peace was never an option.",
     "I did not ask for your trust. I demand only your obedience.", "Regardless of the size of the audience... Always make an entrance... Always Make An Entrance!",
     "I will make you kneel before me!"].sample
   end
 
-  def self.battle(superhero:, villain:)
-    self.catchprases
+  def battle(superhero:, villain:)
+    catchprases
     hero_score = (superhero.power + superhero.combat) + rand(50)
     villain_score = (villain.power + villain.combat) + rand(50)
-    self.update_combat(self.declare_winner(hero_score, villain_score) ? superhero : villain)
+    update_combat(declare_winner(hero_score, villain_score) ? superhero : villain)
   end
 
-  def self.declare_winner(hero_score, villain_score)
+  def declare_winner(hero_score, villain_score)
     puts "Hero score: #{hero_score}"
     puts "Villain score: #{villain_score}"
     if hero_score > villain_score
@@ -80,11 +80,12 @@ class CLI
     end
   end
 
-  def self.update_combat(winner)
+  def update_combat(winner)
     winner.combat += 10
   end
 
-def self.display_villains
+def display_villains
+  #displays the villains leaderboard by power
   Villain.order(power: :desc).each do |villain|
     puts "#{villain.name} | Power: #{villain.power} | Combat: #{villain.combat}"
   end
